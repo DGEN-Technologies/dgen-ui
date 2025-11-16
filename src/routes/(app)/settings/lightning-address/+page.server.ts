aba9e120c9afe8705c3ad1349fc3f3c1e657c302
Kyle 🐆 <kyle@privkey.io>
2025-11-16 13:38:37 -0500

    fix: save settings on LN settings page

--- src/routes/(app)/settings/lightning-address/+page.server.ts
+++ src/routes/(app)/settings/lightning-address/+page.server.ts
@@ -1,6 +1,11 @@
 import type { PageServerLoad } from './$types';
+import updateUser from '$lib/settings';
 
 export const load: PageServerLoad = async ({ parent }) => {
   const { user } = await parent();
   return { user };
+};
+
+export const actions = {
+  default: updateUser,
 };
